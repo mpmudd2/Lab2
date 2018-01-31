92eefb4d2b5e414f42c6f3ec9d4a0d7e8016c428
mpmudd2 <mpmudd2@illinois.edu>
2018-01-30 18:40:37 -0600

    Created fork to submit and implemented code that solved maze

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,6 +35,20 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            while (maze.isFinished() == false) {
+                maze.turnRight();
+                if (maze.canMove() == false) {
+                    maze.turnLeft();
+                    if (maze.canMove() == false) {
+                        maze.turnLeft();
+                        if (maze.canMove() == false) {
+                            maze.turnLeft();
+                        }
+
+                    }
+                }
+                maze.move();
+            }
         }
 
         if (maze.isFinished()) {
